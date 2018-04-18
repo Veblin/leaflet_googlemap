30ed793f844bf03aba4670bf1202e3dfd497ee86
Vivienne <vivienne@ViviennedeMacBook-Pro.local>
2018-04-18 22:52:27 +0800

    完成 leaflet+ googlemap + npm 管理

--- src/app.js
+++ src/app.js
@@ -1,38 +1,29 @@
 import "leaflet";
 import "leaflet.gridlayer.googlemutant";
-var googleMapsClient = require('@google/maps').createClient({
-    key: 'AIzaSyBLiywHBYg_waV3Hh6lH2EQKfTQJpSFYNw',
+
+
+var GoogleMapsLoader = require('google-maps');
+
+GoogleMapsLoader.load(function (google) {
+    var map = L.map('map').setView([51.505, -0.09], 13);
+
+    var roadMutant = L.gridLayer.googleMutant({
+        maxZoom: 18,
+        type: 'roadmap'
+    }).addTo(map);
+
+    // var grid = L.gridLayer({
+    //     attribution: 'Grid Layer',
+    //     //      tileSize: L.point(150, 80),
+    //     //      tileSize: tileSize
+    // });
+
+    // grid.createTile = function (coords) {
+    //     var tile = L.DomUtil.create('div', 'tile-coords');
+    //     tile.innerHTML = [coords.x, coords.y, coords.z].join(', ');
+
+    //     return tile;
+    // };
+    // map.addLayer(grid);
 });
-
-
-// googleMapsClient.geocode({
-//         address: '1600 Amphitheatre Parkway, Mountain View, CA'
-//     })
-//     .asPromise()
-//     .then((response) => {
-//         console.log(response.json.results);
-//     })
-//     .catch((err) => {
-//         console.log(err);
-//     });
-var map = L.map('map').setView([0,0],0);
-
-var roadMutant = L.gridLayer.googleMutant({
-    maxZoom: 24,
-    type:'roadmap'
-}).addTo(map);
-
-var grid = L.gridLayer({
-    attribution: 'Grid Layer',
-//      tileSize: L.point(150, 80),
-//      tileSize: tileSize
-});
-
-grid.createTile = function (coords) {
-    var tile = L.DomUtil.create('div', 'tile-coords');
-    tile.innerHTML = [coords.x, coords.y, coords.z].join(', ');
-
-    return tile;
-};
-
-map.addLayer(grid);
+// GoogleMapsLoader.KEY = 'AIzaSyBLiywHBYg_waV3Hh6lH2EQKfTQJpSFYNw';
